587e27543bc3233911e67d5f185fcfc63b5e40ff
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-04-28 23:32:55 -0400

    prevent zooming

--- scripts.js
+++ scripts.js
@@ -443,7 +443,20 @@ gamepad.addEventListener('touchstart', function(e) {
   }
 });
 
+// Prevent pinch-zoom
+document.addEventListener('gesturestart', function (e) {
+  e.preventDefault();
+});
 
+// Prevent double-tap zoom
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (e) {
+  const now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    e.preventDefault();
+  }
+  lastTouchEnd = now;
+});
 
 //-------------------------------------------------------------
 // Main game setup function
